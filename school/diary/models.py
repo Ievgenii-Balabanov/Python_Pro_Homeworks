2ec82ee41e3b30e7afb72ce8c51e43e6410148e0
Ievgenii <zajazz911@gmail.com>
2023-05-15 00:27:36 +0300

    hw_3 new logic implementation!

--- school/diary/models.py
+++ school/diary/models.py
@@ -8,7 +8,7 @@ class FootballPlayer(models.Model):
     """
     класс FootballPlayer хранящий 4 параметра для создания инстанса
     """
-    name = models.CharField(max_length=20, null=True)
+    name = models.CharField(max_length=20)
     position = models.CharField(max_length=5, null=True)
     transfer_fee = models.IntegerField(default=0)
     club = models.CharField(max_length=3, null=True, blank=True)
--- school/diary/views.py
+++ school/diary/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 import random
 from django.shortcuts import redirect
 from django.shortcuts import render
 
 from .models import FootballPlayer
-
-# Create your views here.
 
 index_template = f"""
     <form action="/login/" method="POST">
@@ -40,6 +38,47 @@ index_template = f"""
     </form>
 """
 
+update_template = f"""
+    <form action="/param_update/" method="POST">
+        <div>
+            <label for="new_item">Enter new name of the player</label>
+            <input name="name" id="new_item" value="" />
+        </div>
+        <p>
+        <div>
+            <label for="new_item">Please specify the position of the player (e.g. GK, CB, CMF, ST, LW, CAM)</label>
+            <input name="position" id="new_position" value="" />
+        </div>
+        <p>
+        <fieldset>
+            <legend>Select new football club:</legend>
+            <div>
+                <input type="radio" id="contactChoice1" name="contact" value="PSG"  />
+                <label for="contactChoice1">PSG</label>
+                <input type="radio" id="contactChoice2" name="contact" value="Parma" />
+                <label for="contactChoice2">Parma</label>
+
+                <input type="radio" id="contactChoice3" name="contact" value="Juventus" />
+                <label for="contactChoice2">Juventus</label>
+            </div>
+        </fieldset>
+        <p>
+        <div>
+            <label for="new_item">Specify new market value</label>
+            <input name="fee" id="new_item" value=""/>
+        </div>
+        <p>
+            <button>Send my choice</button>
+        <p>
+        <div>
+            <a href="/">Return to the HOME page</a>
+        </div>
+    </form>
+"""
+
+
+user_pk = 4
+
 
 def index(request):
     return HttpResponse(index_template)
@@ -71,19 +110,31 @@ def is_exist_check(request):
     функция проверяет существует ли инстанс с указанным pk и если нет - редирект на страницу формы
     """
     try:
-        football_player = FootballPlayer.objects.get(pk=7)
+        football_player = FootballPlayer.objects.get(pk=user_pk)
         return HttpResponse(football_player)
     except Exception:
         return redirect(index)
 
 
 def param_update(request):
-    """
-        изменение параметров инстанса
-        :return:
-    """
-    football_player = FootballPlayer.objects.filter(name="Gatti").update(position="CB", club="Juventus")
-    return HttpResponse(f"Amount of updated instances: {football_player}")
+    if request.POST:
+        football_player = FootballPlayer.objects.get(pk=user_pk)
+        football_player.name = request.POST.get("name")
+        football_player.position = request.POST.get("position")
+        football_player.club = request.POST.get("contact")
+        football_player.transfer_fee = int(request.POST.get('fee'))
+        football_player.save()
+    return HttpResponse(update_template)
+
+
+
+# def param_update(request):
+#     """
+#         изменение параметров инстанса
+#         :return:
+#     """
+#     football_player = FootballPlayer.objects.filter(name="Gatti").update(position="CB", club="Juventus")
+#     return HttpResponse(f"Amount of updated instances: {football_player}")
 
     # #multiple update
     # my_footballplayer = FootballPlayer.objects.get(pk=19)
