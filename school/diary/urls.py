aa8af459da7c20c6a9a0f13ca22331d7a75d4f57
Ievgenii-Balabanov <119660451+Ievgenii-Balabanov@users.noreply.github.com>
2023-05-16 09:54:47 +0300

    Homework 3 (#8)
    * Add blank line
    
    * Add ability to add data from the form into the DB
    
    * DB update
    
    * hw_4 v.2
    
    * hw_4 v.2 add files
    
    * hw_4 v.2 add comments and validators
    
    * hw_4 v.2 add initial migration
    
    * hw_3 added logic implementation!
    
    * hw_3 new logic implementation!
    
    * hw_3 add some changes

--- school/diary/urls.py
+++ school/diary/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
+    path("param_update/", views.param_update, name="param_update"),
+    path("is_exist_check/", views.is_exist_check, name="is_exist_check"),
 ]
--- school/diary/views.py
+++ school/diary/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 import random
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import redirect
+from .models import FootballPlayer
 
 index_template = f"""
     <form action="/login/" method="POST">
@@ -10,54 +9,75 @@ index_template = f"""
             <label for="new_item">Please enter the name of the player</label>
             <input name="name" id="new_item" value="" />
         </div>
+    
         <div>
-            <label for="new_item">Please specify the position of the player (e.g. GK, CB, CM, ST, LW, CAM)</label>
+            <label for="new_item">Please specify the position of the player (e.g. GK, CB, CMF, ST, LW, CAM)</label>
             <input name="position" id="new_position" value="" />
         </div>
-        <button>Submit</button>
         
         <fieldset>
-                <legend>Please select your preferred football club:</legend>
-                <div>
-                    <input type="radio" id="contactChoice1" name="contact" value="PSG"  />
-                    <label for="contactChoice1">PSG</label>
-                    <input type="radio" id="contactChoice2" name="contact" value="Parma" />
-                    <label for="contactChoice2">Parma</label>
-
-                    <input type="radio" id="contactChoice3" name="contact" value="Juventus" />
-                    <label for="contactChoice2">Juventus</label>
-                </div>
-                <div>
-                    <button>Submit</button>
-                </div>
-            </fieldset>
-            
-            <form action="/add_fee" method="POST">
+            <legend>Please select your preferred football club:</legend>
             <div>
-                <label for="new_item">Please specify transfer fee otherwise computer will do it for you</label>
-                <input name="fee" id="new_item" value="" />
-            </div>
-                <button>Send my choice</button>
-    </form>
+                <input type="radio" id="contactChoice1" name="contact" value="PSG"  />
+                <label for="contactChoice1">PSG</label>
+                <input type="radio" id="contactChoice2" name="contact" value="Parma" />
+                <label for="contactChoice2">Parma</label>
 
+                <input type="radio" id="contactChoice3" name="contact" value="Juventus" />
+                <label for="contactChoice2">Juventus</label>
+            </div>
+        </fieldset>
+        
+        <div>
+            <label for="new_item">Please specify transfer fee otherwise computer will do it for you</label>
+            <input name="fee" id="new_item" value="" />
+        </div>
+            <button>Send my choice</button>
+    </form>
+"""
+
+update_template = f"""
+    <form action="/param_update/" method="POST">
+        <div>
+            <label for="new_item">Enter new name of the player</label>
+            <input name="name" id="new_item" value="" />
+        </div>
+        <p>
+        <div>
+            <label for="new_item">Please specify the position of the player (e.g. GK, CB, CMF, ST, LW, CAM)</label>
+            <input name="position" id="new_position" value="" />
+        </div>
+        <p>
+        <fieldset>
+            <legend>Select new football club:</legend>
+            <div>
+                <input type="radio" id="contactChoice1" name="contact" value="PSG"  />
+                <label for="contactChoice1">PSG</label>
+                <input type="radio" id="contactChoice2" name="contact" value="Parma" />
+                <label for="contactChoice2">Parma</label>
+
+                <input type="radio" id="contactChoice3" name="contact" value="Juventus" />
+                <label for="contactChoice2">Juventus</label>
+            </div>
+        </fieldset>
+        <p>
+        <div>
+            <label for="new_item">Specify new market value</label>
+            <input name="fee" id="new_item" value=""/>
+        </div>
+        <p>
+            <button>Send my choice</button>
+        <p>
+        <div>
+            <a href="/">Return to the HOME page</a>
+        </div>
+    </form>
 """
 
 
 def index(request):
     return HttpResponse(index_template)
 
-
-# def login(request):
-#     if request.POST:
-#         name = request.POST.get("name")
-#         position = request.POST.get("position")
-#         club = request.POST.get("contact")
-#         transfer_fee = request.POST.get("transfer_fee")
-#         response = f"Player Name: {name}, Position: {position}, Club: {club}, " \
-#                    f"Transfer fee: {transfer_fee} millions euros"
-#     else:
-#         response = "Please create a player"
-#     return HttpResponse(response)
 
 def login(request):
     if request.POST:
@@ -66,23 +86,38 @@ def login(request):
         club = request.POST.get("contact")
         user_input = False
         try:
-            transfer_fee = request.POST.get('fee')
+            transfer_fee = int(request.POST.get('fee'))
             user_input = True
         except ValueError:
             transfer_fee = random.randrange(1, 100)
         response = f"Player Name: {name}, Position: {position}, Club: {club}, " \
-                   f"Transfer fee {'(added by user): ' if user_input else '(random integer): '}{transfer_fee} millions euros"
+                   f"Transfer fee {'(added by user): ' if user_input else '(random integer): '}" \
+                   f"{transfer_fee} millions euros"
+
+        diary_football_player = FootballPlayer(name=name, position=position, club=club, transfer_fee=transfer_fee)
+        diary_football_player.save()
     else:
         response = "Please create a player"
     return HttpResponse(response)
 
 
-def add_fee(request):
-    user_input = False
+def is_exist_check(request):
+    f"""
+    функция проверяет существует ли инстанс с указанным pk и если нет - редирект на страницу формы
+    """
     try:
-        transfer_fee = request.POST.get('fee')
-        user_input = True
-    except ValueError:
-        transfer_fee = random.randrange(1, 100)
-    response = f"Transfer fee {'(added by user): ' if user_input else '(random integer): '}{transfer_fee} millions euros"
-    return HttpResponse(response)
+        football_player = FootballPlayer.objects.get(pk=1)
+        return HttpResponse(football_player)
+    except Exception:
+        return redirect(index)
+
+
+def param_update(request):
+    if request.POST:
+        football_player = FootballPlayer.objects.get(pk=1)
+        football_player.name = request.POST.get("name")
+        football_player.position = request.POST.get("position")
+        football_player.club = request.POST.get("contact")
+        football_player.transfer_fee = int(request.POST.get('fee'))
+        football_player.save()
+    return HttpResponse(update_template)
